ae32c593e4b911dd753d009e05f3bdbb8d3a71a2
Gabriel Gutanaidze <gutanaidze.gabrieli@gmail.com>
2025-10-02 17:42:18 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+
+
+print("shawarma")
+print("burger")
+print("khinkali")
+print("mwvadi")
+print("khawapuri")
+
+
+print("hello")
+name="gabrieli"
+print(name)
+
+
+print("#########")
+print("# Hello #")
+
+
+
+menu="burger 10$, Coca-Cola 2$, fry 5$"
+print(menu)
