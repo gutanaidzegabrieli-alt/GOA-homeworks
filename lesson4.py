3db3ae43577b381e8adb6bae89b4f199d9255fc0
Gabriel Gutanaidze <gutanaidze.gabrieli@gmail.com>
2025-10-09 20:37:31 +0400

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,24 @@
+age=input("enter your age:")
+print(age)
+
+
+name=input("enter your name:")
+print(name)
+
+
+
+favorite_color=input("enter your favorite color:")
+print(favorite_color)
+
+
+favorite_food=input("enter your favorite food:")
+print(favorite_food)
+
+
+favorite_animal=input("enter your favorite animal:")
+print(favorite_animal)
+
+
+input("bread")
+input("milk")
+input("apples")
